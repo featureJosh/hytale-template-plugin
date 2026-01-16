14b6da4bb1e0018a251a9ffec05be05e53960d08
Josh Q. <jshqntnr13@gmail.com>
2026-01-16 11:48:21 -0500

    test

--- src/main/java/com/mod/nightmaremode/NightmareMode.java
+++ src/main/java/com/mod/nightmaremode/NightmareMode.java
@@ -1,22 +1,48 @@
 package com.mod.nightmaremode;
 
-public class NightmareMode {
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.entity.Entity;
+import com.hypixel.hytale.server.core.entity.entities.player.Player;
+import com.hypixel.hytale.server.core.event.events.EntitySpawnEvent;
+import com.hypixel.hytale.event.SubscribeEvent;
+
+public class NightmareMode extends JavaPlugin {
 
     private static NightmareMode instance;
-    
-    public NightmareMode() {
+
+    public NightmareMode(JavaPluginInit init) {
+        super(init);
         instance = this;
-        System.out.println("[Nightmare Mode] Plugin loaded!");
     }
-    
-    public void onEnable() {
-        System.out.println("[Nightmare Mode] Plugin enabled!");
+
+    @Override
+    public void setup() {
+        getEventRegistry().register(this);
     }
-    
-    public void onDisable() {
-        System.out.println("[Nightmare Mode] Plugin disabled!");
+
+    @Override
+    public void start() {
+        getLogger().info("Nightmare Mode enabled!");
     }
-    
+
+    @Override
+    public void shutdown() {
+        getLogger().info("Nightmare Mode disabled!");
+    }
+
+    @SubscribeEvent
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        Entity entity = event.getEntity();
+
+        if (!(entity instanceof Player)) {
+            double healthMultiplier = 4;
+            double baseHealth = entity.getMaxHealth();
+            entity.setMaxHealth(baseHealth * healthMultiplier);
+            entity.setHealth(entity.getMaxHealth());
+        }
+    }
+
     public static NightmareMode getInstance() {
         return instance;
     }
